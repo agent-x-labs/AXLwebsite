1f0c48963b83e2a6d5dee1e97bc505bc06db748c
alex <alexlomt@yahoo.com>
2026-03-30 14:41:31 +0100

    Set company registration number

--- src/legal/companyDetails.js
+++ src/legal/companyDetails.js
@@ -3,7 +3,7 @@ export const companyDetails = {
   email: 'info@agentxlabs.com',
   address: 'F3 Bench, 22 Kings Bench Street, SE1 0QX London, United Kingdom',
   placeOfRegistration: 'England and Wales',
-  companyRegistrationNumber: '[Company registration number to be added before launch]',
+  companyRegistrationNumber: '17105341',
 }
 
 export const legalLastUpdated = '30 March 2026'
